754651cb1d8d927842f0817f9650bc069dc756c0
phu-bui <phu-bui@github.com>
2020-08-01 13:35:40 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, flash, redirect, render_template, request
-from fitbert import FitBert
+from flask import Flask, render_template, request
 from predict import FillBert
+from fitbert import FitBert
 import requests
 import re
 app = Flask(__name__)
@@ -10,31 +10,22 @@ fb = FitBert()
 def home():
     return render_template('index.html')
 
-@app.route('/result', methods = ['POST'])
+@app.route('/result', methods=['POST'])
 def result():
     questions = request.form['question'].replace('\r', '').split('\n')
     row = {}
     row['origin'] = request.form['question'].replace('\n', '<br>')
     row['question'] = questions[0].strip()
     print(questions)
-    for i in range(1, 5):
-        row[str(i)] = re.sub('</?.*?>', '', questions[i].replace('(', '<').replace(')', '>')).strip()
+    for i in range(1,5):
+        # print(questions[i])
+        row[str(i)] = re.sub('</?.*?>','',questions[i].replace('(','<').replace(')', '>')).strip()
     masked_string = row['question'].replace('___', '***mask***')
     options = [row['1'], row['2'], row['3'], row['4']]
     row['answer'] = fb.rank_with_prob(masked_string, options)[0][0]
-    return render_template('result.html')
-
+    return render_template('result.html', row=row)
 @app.route('/example')
 def example():
-    return  render_template('example.html')
-
+    return render_template('example.html')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4040, debug=True, threaded=True)
-
-
-
-
-
-
-
-
+     app.run(host='0.0.0.0', port=4000, debug=True, threaded=True)
